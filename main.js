7195fdf01adf8a502e4ddb0098d6c917d3a4016e
B1scuit <dysvir@mail.ru>
2017-06-29 20:03:06 +0100

    Signed-off-by: B1scuit <dysvir@mail.ru>

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
-
 var config = fs.readFileSync("config.json");
-var data = data = JSON.parse(config);
-
+var data2 = JSON.parse(config);
+var data;
 function dirload(dir) {
 
   var files = fs.readdirSync(dir)
@@ -14,20 +13,49 @@ function dirload(dir) {
   return JSON.parse(fsdata);
 }
 
-data["map"] = dirload('maps/');
-data["mode"] = dirload('modes/');
-
-data["blessings"] = [];
-data["curses"] = [];
-
-for(var i=1; i <= data.blessings; i++) {
-  data["blessings"][i] = dirload('blessings/');
+function arraySort(val, index, arr){
+	var key = val.split(" ");
+	if(key[1] != null){
+		configFile[key[0]] = key[1];
+	} else {
+		configFile[key[0]] = "";
+	}
 }
 
-for(var i=1; i <= data.curses; i++) {
-  data["curses"][i] = dirload('curses/');
+function arrayMerge(val, index){
+	configFile[index] = val;
 }
 
-console.log(data);
+// load the configs
+var configFile = [];
 
-cmd = data["map"].cmd.join('\n');
+var mode = dirload('modes/');
+var modeCmd = mode.cmd
+modeCmd.forEach(arraySort);
+console.log(mode.name)
+
+for(var i = 0;i < 4; i++){
+	data = dirload('tumblers/');
+	var rand = dirload('tumblers/').cmd
+	rand.forEach(arraySort);
+	console.log(data.name)
+}
+
+// Add anything else we need 
+configFile["mp_restartgame"] = "1";
+
+var file = fs.createWriteStream(data2.csgoConfigDir + "/csgo/cfg/rouletteGame.cfg");
+file.on('error', function(err) { console.log(err) });
+
+var str;
+for (var k in configFile){
+    if (configFile.hasOwnProperty(k)) {
+        file.write(k + " " + configFile[k]+ '\n');
+    }
+}
+
+file.end();
+
+console.log("\nPlease type \"exec rouletteGame\" in your session");
+
+
